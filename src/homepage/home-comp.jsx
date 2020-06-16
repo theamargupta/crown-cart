eceb9ec10f9f1487fa476ec749624175a823978e
Amar Gupta <amar9911056196@gmail.com>
2020-06-16 11:21:43 +0530

    update

---
+++ src/homepage/home-comp.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+const homePage = () => {
+    <div className="homePage">
+        <div className="dir-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Hats</h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Jackets</h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Sneakers</h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Womens</h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">Mens</h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+}
