956471f7a4fe6eff27c365f74054aba87b03c71d
Amar Gupta <amar9911056196@gmail.com>
2020-06-17 00:40:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/home-comp'
+import React from "react";
+import { Route } from "react-router-dom";
+
+import "./App.css";
+import HomePage from "./pages/homepage/home-comp";
 
 function App() {
-  return (<div>
-      <HomePage />
-    </div>)
-  
+  return (
+    <div>
+      <Route exact path="/" component={HomePage} />
+    </div>
+  );
 }
 
 export default App;
+//video 3
