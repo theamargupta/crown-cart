e7ab4edcb247cf5dcba7155a81f7bd4275525887
Amar Gupta <amar9911056196@gmail.com>
2020-06-16 23:15:04 +0530

    broken our webpage

--- src/component/menu-item/menu-item-comp.jsx
+++ src/component/menu-item/menu-item-comp.jsx
@@ -3,11 +3,15 @@ import React from "react";
 import "./meny-item-style.scss";
 
 const MenuItem = ({ title, imageUrl }) => (
-  <div style={{
-    backgroundImage: `url(${imageUrl})`
-  }}className="menu-item">
+  <div className="menu-item">
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
     <div className="content">
-      <div className="title">{title}</div>
+      <div className="title">{title.toUpperCase()}</div>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
