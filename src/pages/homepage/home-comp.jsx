c376d985886d5d289407caa809b05a87830a25ce
Amar Gupta <amar9911056196@gmail.com>
2020-06-16 22:07:18 +0530

    update

--- src/pages/homepage/home-comp.jsx
+++ src/pages/homepage/home-comp.jsx
@@ -1,37 +1,10 @@
 import React from 'react';
 
 import './homepage.styles.scss';
-
+import Directory from "../../component/directory/dir-comp"
 const HomePage = () => (
   <div className='homepage'>
-    <h1>Welcome to my Homepage</h1>
-    <div className='directory-menu'>
-      
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>JACKETS</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>SHOES</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>WOMENS</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <div className='title'>MENS</div>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
