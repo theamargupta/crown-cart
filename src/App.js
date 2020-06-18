45d133fbf8ea91359ec1ef658604bd90a61b46e3
Amar Gupta <amar9911056196@gmail.com>
2020-06-18 16:50:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import './App.css';
 import HomePage from './pages/homepage/home-comp';
 import ShopPage from './pages/shop/shop-comp';
+import Header from './component/header/header-comp';
+
+import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
